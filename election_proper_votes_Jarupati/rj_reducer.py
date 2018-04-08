71ddc0092e8615c0d283ba3028f4ee31d1b61c5c
raghavender jarupati <raghavender.jarupati@mail.nwmissouri.edu>
2018-04-08 18:55:41 -0500

    Added console screenshots

--- election_proper_votes_Jarupati/rj_reducer.py
+++ election_proper_votes_Jarupati/rj_reducer.py
@@ -14,13 +14,13 @@ for line in s:
     if settlement_name != thisKey:
         if thisKey and thisKey!="": 
              r.write(thisKey + '\t' + min(num_list)+ '\n')
-             #print settlement_name+'\t'+'Minumum: ' +min(num_list)
+             print settlement_name+'\t'+'Minumum: ' +min(num_list)
         thisKey = settlement_name # initializing values to default again
         thisValue = 0.0 
         num_list = [] # empty the list each and every time it is a new key value
     num_list.append(votes) # list to store the values of each classification
-#print '\n'
+print '\n'
 r.write(thisKey + '\t' +  str(minValue) +'\n') 
-#print settlement_name+'\t'+'Minumum: ' +min(num_list) # to print the values in console
+#print settlement_name+'\t'+min(num_list) # to print the values in console
 
 
