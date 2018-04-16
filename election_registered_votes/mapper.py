8a5550ae510fc78ac61e4df1fad4b8a668971560
iakshayy1 <vontariakshayreddy184@gmail.com>
2018-04-15 19:46:07 -0500

    Images/Registered_votes.png added

--- election_registered_votes/mapper.py
+++ election_registered_votes/mapper.py
@@ -1,20 +1,20 @@
 f = open("data.txt","r")  # open file, read-only raw data
 o = open("outputRegVotes.txt", "w") # open file, write - just our key, value pairs
 for line in f:  
-    data = line.strip().split(",") 
+    data = line.strip().split(",") # splits the data and formats
     if len(data) == 40:
         settlement_name,settlement_code,settlement_name_english,booth_number,registered_voters,votes,bad_votes,proper_votes,Ale_Yarok,Am_Shalem,Balad,Brit_Olam,Daam_Workers_Party,Eretz_Hadasha,Green_Party,Hadash,Haim_Bekavod,Hatnua,Hope_Change,HaYisraelim,Kadima,Kalkala,Koah_LeHashpia,The_Jewish_Home,Labour_Party,Leader,Likud_Beitenu,Meretz,Moreshet_Avot,Na_Nach,Or,Otzma_LeYisrael,Pirate_Party,Raam_Taal,Senior_Citizens_Party,Shas,Social_Justice,United_Torah_Judaism,We_Brothers,Yesh_Atid = data
-        o.write("{0}\t{1}\n".format(settlement_name_english, registered_voters))
+        o.write("{0}\t{1}\n".format(settlement_name_english, registered_voters)) # write the required data into the outputRegVotes file
 
 f.close()
 o.close()
 
 n = open("outputRegVotes.txt","r")  # open file, read-only
 s = open("sorted.txt", "w") # open file, write
-lines = n.readlines()
-lines.sort()
+lines = n.readlines() # reads all the lines in the files
+lines.sort() # all the lines are sort using sort() method
 
 for line in lines:
-	s.write(line)
+	s.write(line) # write the sorted data into the sorted.txt using the open()
 n.close()
 s.close()
--- election_registered_votes/reducer.py
+++ election_registered_votes/reducer.py
@@ -6,13 +6,13 @@ value = 0
 count = 0
 
 for line in s:
-    data = line.strip().split('\t')
-    settlement_name, registered_voters = data
+    data = line.strip().split('\t') # splits the data and formats
+    settlement_name, registered_voters = data # gets the required data into the data variable
     if settlement_name != key:
         if key:
-            r.write(key + '\t' + str(value)+'\n')
+            r.write(key + ',' + str(value)+'\n') ## write the required data into the reducedData file
 
         key = settlement_name
         value = 0
-    value += int(registered_voters)
+    value += int(registered_voters) # value variable contains all the total of the registered_voters
     
